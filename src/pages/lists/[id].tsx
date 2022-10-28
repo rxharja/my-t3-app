f960462150338a9506468fa227ebb5ba38b51537
Redon Xharja <redon.xharja@gmail.com>
2022-10-28 19:36:47 -0400

    fixed state rendering problem

--- src/pages/lists/[id].tsx
+++ src/pages/lists/[id].tsx
@@ -1,49 +1,58 @@
 import { TodoItem } from "@prisma/client";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useState } from "react";
 import { match, P } from "ts-pattern";
 import { trpc } from "../../utils/trpc";
 
 const TodoList: NextPage = () => {
   const id = useRouter().query.id as string;
 
-  const [todoList, setTodoList] = useState(
-    trpc.todoLists.getList.useQuery({
-      id: id
-    }).data
+  const {
+    data: todoList,
+    refetch,
+    isLoading,
+  } = trpc.todoLists.getList.useQuery(
+    { id: id },
+    {
+      refetchInterval: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    }
   );
 
   const setItemCompletion = trpc.todoLists.updateItemCompletion.useMutation();
 
   const complete = async (item: TodoItem) => {
-    const list = (
-      await setItemCompletion.mutateAsync({
-        id: item.id,
-        done: !item.done
-      })
-    )?.TodoList;
+    await setItemCompletion.mutateAsync({
+      id: item.id,
+      done: !item.done,
+    });
 
-    setTodoList(list);
+    await refetch();
   };
 
-  return (
-    <>
-      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
-        <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
-          <span className="text-purple-300">{todoList?.name}</span>
-        </h1>
-        <div className="flex w-full flex-col items-center justify-center pt-6 text-2xl text-blue-500"></div>
-        {match(todoList)
-          .with(P.nullish, { TodoItems: [] }, () => <p>No Items</p>)
-          .with({ TodoItems: P.select("itms") }, ({ itms }) => itms.map((itm) => (
-            <div key={itm.id} onClick={() => complete(itm)}>
-              <Todo item={itm} />
-            </div>
-          ))).exhaustive()}
-      </main>
-    </>
-  );
+  if (isLoading) return <p>Loading...</p>;
+  else
+    return (
+      <>
+        <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+          <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
+            <span className="text-purple-300">{todoList?.name}</span>
+          </h1>
+          <div className="flex w-full flex-col items-center justify-center pt-6 text-2xl text-blue-500"></div>
+          {match(todoList)
+            .with(P.nullish, { TodoItems: [] }, () => <p>No Items</p>)
+            .with({ TodoItems: P.select("itms") }, ({ itms }) =>
+              itms.map((itm) => (
+                <div key={itm.id} onClick={() => complete(itm)}>
+                  <Todo item={itm} />
+                </div>
+              ))
+            )
+            .exhaustive()}
+        </main>
+      </>
+    );
 };
 
 const Todo: React.FC<{ item: TodoItem }> = ({ item }) => (
