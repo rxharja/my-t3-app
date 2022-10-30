c4153a4d7dce7bc244c4b796f923074e106acc1f
Redon Xharja <redon.xharja@gmail.com>
2022-10-30 19:18:30 -0400

    added ability to add items to a list

--- src/pages/lists/[id].tsx
+++ src/pages/lists/[id].tsx
@@ -1,6 +1,7 @@
 import { TodoItem } from "@prisma/client";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { match, P } from "ts-pattern";
 import { trpc } from "../../utils/trpc";
 
@@ -50,6 +51,7 @@ const TodoList: NextPage = () => {
               ))
             )
             .exhaustive()}
+          <AddItem refetch={refetch} listId={id} />
         </main>
       </>
     );
@@ -64,5 +66,67 @@ const Todo: React.FC<{ item: TodoItem }> = ({ item }) => (
     {item.name}
   </p>
 );
+
+const AddItem = ({
+  refetch,
+  listId,
+}: {
+  refetch: () => void;
+  listId: string;
+}) => {
+  const [name, setName] = useState("");
+  const [add, setAdd] = useState(true);
+  const addItemMutation = trpc.todoLists.addItem.useMutation();
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setAdd(true);
+    setName("");
+    await addItemMutation.mutateAsync({ name, listId });
+    await refetch();
+  };
+
+  return (
+    <>
+      {add && (
+        <button
+          className="m-2 h-12 w-12 rounded-xl bg-purple-300 text-gray-100"
+          onClick={() => setAdd(false)}
+        >
+          +
+        </button>
+      )}
+      {!add && (
+        <>
+          <form onSubmit={onSubmit}>
+            <div className="flex flex-row">
+              <div className="mx-12"></div>
+              <input
+                className="w-32 border-b-2 border-b-gray-500"
+                type="text"
+                id="list-name"
+                name="listName"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <button
+                className="rounded-xl bg-purple-100 p-2 text-gray-500"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          <button
+            className="m-2 h-12 w-12 rounded-xl bg-purple-300 text-gray-100"
+            onClick={() => setAdd(true)}
+          >
+            -
+          </button>
+        </>
+      )}
+    </>
+  );
+};
 
 export default TodoList;
